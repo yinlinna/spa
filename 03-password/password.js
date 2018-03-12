76224e843af40e146d4f8696812553542f25669b
阴琳娜 <703913979@qq.com>
2018-03-12 22:18:52 +0800

    Create password.js

---
+++ 03-password/password.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".img").click(function(){
+    if($("#password").attr("type")=="password"){
+      $("#password").attr("type","text");
+    }else{
+      $("#password").attr("type","password");
+    }
+  })
+});
