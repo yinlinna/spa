7a4db78d01efbc2d3300c30903cc2244b3438239
yinlinna <703913979@qq.com>
2018-03-13 16:56:57 +0800

    1

--- 04-range/range.js
+++ 04-range/range.js
@@ -1,12 +1,10 @@
-*{
-	margin: 10px;
-	font-size: 16px;
-	text-align: center;
-}
-#btn{
-	width: 100px;
-	height: 50px;
-}
-#age{
-	font-weight: bold;
-}
+$(function(){
+	var $ran = $("#range");
+	var $age = $("#age");
+	$age.html('0');
+  
+	$("#range").change(function(){
+	  $age.html($ran.val());
+	});
+  
+  });
